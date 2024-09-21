13c8188f7bd6c17af85dc8103f7e28e19450144d
Arslan Ahmad <arslanmoosa328@gmail.com>
2024-09-22 02:38:30 +0500

    j

--- terraform/resource-group.tf
+++ terraform/resource-group.tf
@@ -1,9 +1,5 @@
-data "azurerm_resource_group" "existing_ampart" {
-  name = "ampart5"
-}
 
 resource "azurerm_resource_group" "ampart5" {
-  count = length(data.azurerm_resource_group.existing_ampart.name) == 0 ? 1 : 0
   name     = "ampart5"
   location = var.location
 }
