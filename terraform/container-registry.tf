ca21d6736bd8d5a183f6559bbae0c4a2bd0c40f1
Arslan Ahmad <arslanmoosa328@gmail.com>
2024-09-22 02:43:22 +0500

    h

--- terraform/container-registry.tf
+++ terraform/container-registry.tf
@@ -1,10 +1,5 @@
-data "azurerm_container_registry" "existing_registry" {
-  name                = var.app_name
-  resource_group_name = azurerm_resource_group.ampart5.name
-}
 
 resource "azurerm_container_registry" "container_registry" {
-  count               = length(data.azurerm_container_registry.existing_registry.name) == 0 ? 1 : 0
   name                = var.app_name
   resource_group_name = azurerm_resource_group.ampart5.name
   location            = var.location
--- terraform/kubernetes-cluster.tf
+++ terraform/kubernetes-cluster.tf
@@ -1,25 +1,18 @@
-data "azurerm_kubernetes_cluster" "existing_cluster" {
-  name                = var.app_name
-  resource_group_name = azurerm_resource_group.ampart5.name
-}
-
 resource "azurerm_kubernetes_cluster" "cluster" {
-  count = length(data.azurerm_kubernetes_cluster.existing_cluster.name) == 0 ? 1 : 0
-  name                = var.app_name
-  location            = var.location
-  resource_group_name = azurerm_resource_group.ampart5.name
-  dns_prefix          = var.app_name
-  kubernetes_version  = var.kubernetes_version
+    name                = var.app_name
+    location            = var.location
+    resource_group_name = azurerm_resource_group.ampart5.name  # Ensure this matches
+    dns_prefix          = var.app_name
+    kubernetes_version  = var.kubernetes_version
 
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_DS2_v2"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
+    default_node_pool {
+        name            = "default"
+        node_count      = 1
+        vm_size         = "Standard_DS2_v2"
+    }
+    identity {
+        type = "SystemAssigned"
+    }    
 }
 
 resource "azurerm_role_assignment" "role_assignment" {
