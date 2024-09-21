bf32894f8c97a8cd6c6f7749c1f0d4688fadc866
Arslan Ahmad <arslanmoosa328@gmail.com>
2024-09-22 02:09:34 +0500

    .tf updates

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,3 @@
-output "djb993rg" {
-  value = "${azurerm_resource_group.djb993rg.name}"
-}
-
-output "djb993cr" {
-  value = "${azurerm_container_registry.djb993cr.name}"
-}
 
 output "aks-cluster" {
   value = "${azurerm_kubernetes_cluster.aks-cluster.name}"
